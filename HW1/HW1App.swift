a86e77ddcdfbf3fabddd488c52ca90395d347341
Ace <sean3136@gmail.com>
2022-09-21 15:40:03 +0800

    Initial Commit

---
+++ HW1/HW1App.swift
@@ -0,0 +1,17 @@
+//
+//  HW1App.swift
+//  HW1
+//
+//  Created by 李炘杰 on 2022/9/21.
+//
+
+import SwiftUI
+
+@main
+struct HW1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
